2036c743e7684b72529d597e54b8dc813a8940c4
mballance-sf <matt.ballance@gmail.com>
2018-07-09 13:16:38 -0700

    Update ivpm support

--- scripts/ivpm.mk
+++ scripts/ivpm.mk
@@ -35,9 +35,16 @@ RULES := 1
 
 ifeq (true,$(PHASE2))
 build : $(BUILD_DEPS)
+
+clean :
+	$(Q)rm -rf $(LIB_DIR)
+
 else
 build : $($(PROJECT)_deps)
 	$(Q)$(MAKE) -f $(SCRIPTS_DIR)/ivpm.mk PHASE2=true VERBOSE=$(VERBOSE) build
+
+clean : $($(PROJECT)_clean_deps)
+	$(Q)$(MAKE) -f $(SCRIPTS_DIR)/ivpm.mk PHASE2=true VERBOSE=$(VERBOSE) clean
 endif
 
 # TODO: Change Makefile targets
